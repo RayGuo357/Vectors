2d9c8c992c1e9a51fa59e49a545a1007aaf22f31
Raymond Guo <rmguo@live.unc.edu>
2020-07-06 18:47:28 -0400

    Created class for vectors

---
+++ vectors.py
@@ -0,0 +1,21 @@
+class Vector():
+    def __init__(self, a: int, b: int, c: int):
+        if not isinstance(a, int):
+            raise TypeError("'a' must be an integer")
+        elif not isinstance(b, int):
+            raise TypeError("'b' must be an integer")
+        elif not isinstance(c, int):
+            raise TypeError("'c' must be an integer")
+        else:
+            self.__a = a
+            self.__b = b
+            self.__c = c
+
+    def getA(self):
+        return self.__a
+
+    def getB(self):
+        return self.__b
+
+    def getC(self):
+        return self.__c
