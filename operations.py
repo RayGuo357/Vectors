5590acdf3dcd1d9e64ae55c0c2f52c1e098fd651
Raymond Guo <rmguo@live.unc.edu>
2020-07-06 19:18:18 -0400

    Finished cross product method

--- operations.py
+++ operations.py
@@ -12,5 +12,13 @@ def dotProduct(v1: Vector, v2: Vector) -> int:
         product += v1.getC() * v2.getC()
         return product
 
-def crossProduct(v1: Vector, v2: Vector) -> int:
-    return
+def crossProduct(v1: Vector, v2: Vector) -> Vector:
+    if not isinstance(v1, Vector):
+        raise TypeError("v1 must be a vector")
+    elif not isinstance(v2, Vector):
+        raise TypeError("v2 must be a vector")
+    else:
+        x = (v1.getY() * v2.getZ()) - (v1.getZ() * v2.getY())
+        y = (v1.getZ() * v2.getX()) - (v1.getX() * v2.getZ())
+        z = (v1.getX() * v2.getY()) - (v1.getY() * v2.getX())
+        return Vector(x, y, z)
