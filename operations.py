c1b38c16285acb80effd7202081dfbf6cf7fa3e3
Raymond Guo <rmguo@live.unc.edu>
2020-07-06 19:28:11 -0400

    Updated naming conventions

--- operations.py
+++ operations.py
@@ -7,9 +7,9 @@ def dot_product(v1: Vector, v2: Vector) -> int:
         raise TypeError("v2 must be a vector")
     else:
         product = 0
-        product += v1.get_a() * v2.get_a()
-        product += v1.get_b() * v2.get_b()
-        product += v1.get_c() * v2.get_c()
+        product += v1.get_x() * v2.get_x()
+        product += v1.get_y() * v2.get_y()
+        product += v1.get_z() * v2.get_z()
         return product
 
 def cross_product(v1: Vector, v2: Vector) -> Vector:
