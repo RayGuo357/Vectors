d0c076adf74a0a93f5a9320c2bac278e422c8fe0
Raymond Guo <rmguo@live.unc.edu>
2020-07-06 21:41:20 -0400

    Added Point class

---
+++ points.py
@@ -0,0 +1,36 @@
+class Point():
+    def __init__(self, x: int, y: int, z:int):
+        if not isinstance(x, int):
+            raise TypeError("'x' must be an integer")
+        elif not isinstance(y, int):
+            raise TypeError("'y' must be an integer")
+        elif not isinstance(z, int):
+            raise TypeError("'z' must be an integer")
+        else:
+            self.__x = x
+            self.__y = y
+            self.__z = z
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def get_z(self):
+        return self.__z
+
+    def set_x(self, x):
+        self.__x = x
+
+    def set_y(self, y):
+        self.__y = y
+
+    def set_z(self, z):
+        self.__z = z
+
+    def print_point(self):
+        print("(" + str(self.get_x()) + ", " + str(self.get_y()) + ", " + str(self.get_z()) + ")")
+
+    def to_array(self):
+        return [self.get_x(), self.get_y(), self.get_z()]
