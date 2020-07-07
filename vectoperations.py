920cdfdf68cc58549f430290eb39451c696f6bf9
Raymond Guo <rmguo@live.unc.edu>
2020-07-06 21:02:44 -0400

    Removed unnecessary function

--- vectoperations.py
+++ vectoperations.py
@@ -31,12 +31,16 @@ def dot_product(v1, v2) -> int:
     if not (isinstance(v2, Vector) or isinstance(v2, list)):
         raise TypeError("v2 must be an object of Vector or an array")
 
-    assign_var(v1, v2)
+    if isinstance(v1, list):
+        v1  = Vector(v1[0], v1[1], v1[2])
+
+    if isinstance(v2, list):
+        v2 = Vector(v2[0], v2[1], v2[2])
 
     product = 0
-    product += x1 * x2
-    product += y1 * y2
-    product += z1 * z2
+    product += v1.get_x() * v2.get_x()
+    product += v1.get_y() * v2.get_y()
+    product += v1.get_z() * v2.get_z()
     return product
 
 # Function for cross product of 2 vectors
@@ -46,9 +50,16 @@ def cross_product(v1, v2) -> Vector:
     if not (isinstance(v2, Vector) or isinstance(v2, list)):
         raise TypeError("v2 must be an object of Vector or an array")
 
-    assign_var(v1, v2)
+    if isinstance(v1, list):
+        v1  = Vector(v1[0], v1[1], v1[2])
 
-    x = (y1 * z2) - (z1 * y2)
-    y = (z1 * x2) - (x1 * z2)
-    z = (x1 * y2) - (y1 * x2)
+    if isinstance(v2, list):
+        v2 = Vector(v2[0], v2[1], v2[2])
+
+    x = (v1.get_y() * v2.get_z()) - (v1.get_z() * v2.get_y())
+    y = (v1.get_z() * v2.get_x()) - (v1.get_x() * v2.get_z())
+    z = (v1.get_x() * v2.get_y()) - (v1.get_y() * v2.get_x())
     return Vector(x, y, z)
+
+print(dot_product([1,2,3],[1,5,7]))
+cross_product([1,2,3],[1,5,7]).print_vector()
