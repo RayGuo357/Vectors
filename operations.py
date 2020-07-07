3f5f55df8ea72db2424d52c086f2a38e3de0b530
Raymond Guo <rmguo@live.unc.edu>
2020-07-06 20:11:56 -0400

    Added functionality for array or obj of Vector

--- operations.py
+++ operations.py
@@ -1,24 +1,55 @@
 from vectors import Vector
 
-def dot_product(v1: Vector, v2: Vector) -> int:
-    if not isinstance(v1, Vector):
-        raise TypeError("v1 must be a vector")
-    elif not isinstance(v2, Vector):
-        raise TypeError("v2 must be a vector")
-    else:
-        product = 0
-        product += v1.get_x() * v2.get_x()
-        product += v1.get_y() * v2.get_y()
-        product += v1.get_z() * v2.get_z()
-        return product
+def assign_var(v1, v2):
+    global x1
+    global y1
+    global z1
 
-def cross_product(v1: Vector, v2: Vector) -> Vector:
-    if not isinstance(v1, Vector):
-        raise TypeError("v1 must be a vector")
-    elif not isinstance(v2, Vector):
-        raise TypeError("v2 must be a vector")
+    global x2
+    global y2
+    global z2
+
+    if isinstance(v1, list):
+        x1 = v1[0]
+        y1 = v1[1]
+        z1 = v1[2]
     else:
-        x = (v1.get_y() * v2.get_z()) - (v1.get_z() * v2.get_y())
-        y = (v1.get_z() * v2.get_x()) - (v1.get_x() * v2.get_z())
-        z = (v1.get_x() * v2.get_y()) - (v1.get_y() * v2.get_x())
-        return Vector(x, y, z)
+        x1 = v1.get_x()
+        y1 = v1.get_y()
+        z1 = v1.get_z()
+
+    if isinstance(v2, list):
+        x2 = v2[0]
+        y2 = v2[1]
+        z2 = v2[2]
+    else:
+        x2 = v2.get_x()
+        y2 = v2.get_y()
+        z2 = v2.get_z()
+
+def dot_product(v1, v2) -> int:
+    if not (isinstance(v1, Vector) or isinstance(v1, list)):
+        raise TypeError("v1 must be an object of Vector or an array")
+    if not (isinstance(v2, Vector) or isinstance(v2, list)):
+        raise TypeError("v2 must be an object of Vector or an array")
+
+    assign_var(v1, v2)
+
+    product = 0
+    product += x1 * x2
+    product += y1 * y2
+    product += z1 * z2
+    return product
+
+def cross_product(v1, v2) -> Vector:
+    if not (isinstance(v1, Vector) or isinstance(v1, list)):
+        raise TypeError("v1 must be an object of Vector or an array")
+    if not (isinstance(v2, Vector) or isinstance(v2, list)):
+        raise TypeError("v2 must be an object of Vector or an array")
+
+    assign_var(v1, v2)
+
+    x = (y1 * z2) - (z1 * y2)
+    y = (z1 * x2) - (x1 * z2)
+    z = (x1 * y2) - (y1 * x2)
+    return Vector(x, y, z)
